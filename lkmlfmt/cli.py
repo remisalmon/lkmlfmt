952d834601e5a4c7057459a4ac0dac913f3dea8d
Remi Salmon <salmon.remi@gmail.com>
2024-07-29 17:08:05 -0600

    Push code

---
+++ lkmlfmt/cli.py
@@ -0,0 +1,20 @@
+import sys
+import lkml
+
+
+def cli():
+    if len(sys.argv) == 1:
+        print("usage: lkmlfmt file ...")
+
+        sys.exit(1)
+
+    for file in sys.argv[1:]:
+        print("formatting {}".format(file))
+
+        with open(file, mode="r+") as file_object:
+            data = lkml.load(file_object)
+            file_object.seek(0)
+            lkml.dump(data, file_object)
+            file_object.truncate()
+
+    return
